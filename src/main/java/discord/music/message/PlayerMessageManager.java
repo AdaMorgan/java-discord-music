4ca76a44b93ceb8e3d0d6522dc3291b33e974a9e
Ada Morgan <54023403+AdaMorgan@users.noreply.github.com>
2023-06-01 08:09:04 +0300

    update message

--- src/main/java/discord/music/message/PlayerMessageManager.java
+++ src/main/java/discord/music/message/PlayerMessageManager.java
@@ -102,6 +102,7 @@ public class PlayerMessageManager implements AutoCloseable {
 			case "soundcloud" -> "soundcloud";
 			case "apple" -> "apple";
 			case "twitch" -> track.getInfo().artworkUrl;
+			case "yandex" -> "yandex";
 			default -> "audio";
 		};
 	}
