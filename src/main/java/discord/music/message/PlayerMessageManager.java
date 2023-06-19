f621daa18eb1dc10784e8ee4c95aec3a0da15d3d
adamorgan <zanna6301@gmail.com>
2023-06-19 12:41:55 +0300

    update

--- src/main/java/discord/music/message/PlayerMessageManager.java
+++ src/main/java/discord/music/message/PlayerMessageManager.java
@@ -150,8 +150,7 @@ public class PlayerMessageManager implements AutoCloseable {
 				ActionRow.of(
 						ButtonType.LOOP_TRACK.getButton().withStyle(getStyle(!scheduler.isLoopTrack())).withDisabled(!track.isSeekable()),
 						ButtonType.LOOP_QUEUE.getButton().withStyle(getStyle(!scheduler.isLoopQueue())).withDisabled(!track.isSeekable()),
-						ButtonType.SHUFFLE.getButton().withDisabled(!track.isSeekable()),
-						ButtonType.SEARCH.getButton().withDisabled(!track.isSeekable())
+						ButtonType.SHUFFLE.getButton().withDisabled(!track.isSeekable())
 				));
 	}
 
